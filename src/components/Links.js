9625e47f4ddfaf5023811bf77b0727a389d7fab1
Jesse Olsen <jesse.olsen@hpe.com>
2021-02-04 00:51:40 -0700

    Refactor out Table and Links components

---
+++ src/components/Links.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Links() {
+  return (
+    <>
+      <a
+        className="App-link"
+        href="https://jesseolsen.github.io/charter/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Hosted site
+      </a>
+      <a
+        className="App-link"
+        href="https://github.com/jesseolsen/charter"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub source repository
+      </a>
+    </>
+  );
+}
+
+export default Links;
